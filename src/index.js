e925549c1404b8e1aa702376aeb925cdbbc45e85
Exist95 <jyeol0210@naver.com>
2023-01-23 22:31:28 +0900

    초안

--- src/index.js
+++ src/index.js
@@ -1,1 +1,56 @@
-/** @param {any[]} arr */
+/**
+ * @description 회원가입 하는 함수
+ * @param {string} nickname
+ * @param {string} email
+ * @param {string} password
+ * @returns {object} message 및 토큰
+ */
+const signUp = (nickname, email, password) => {};
+
+/**
+ * @description 로그인 하는 함수
+ * @param {string} email
+ * @param {string} password
+ * @returns {object} message 및 토큰
+ */
+const login = (eamil, password) => {};
+
+/**
+ * @description 할 일 만들기
+ * @param {string} title
+ * @param {string} content
+ * @header Authorization: login token
+ * @returns {object} title, content, id, createAt, updateAt
+ */
+const createTodo = (title, content) => {};
+
+/**
+ * @description 할 일 불러오기
+ * @header Authorization: login token
+ * @return {object} All Todo data
+ */
+const getTodos = () => {};
+
+/**
+ * @description 할 일 수정
+ * @param {string} title
+ * @param {string} content
+ * @header Authorization: login token
+ * @returns {object} title, content, id, createAt, updateAt
+ */
+const updateTodo = (title, content) => {};
+
+/**
+ * @description 할 일 삭제
+ * @header Authorization: login token
+ * @returns {object} null
+ */
+const deleteTodo = () => {};
+
+/**
+ * @description 특정 Todo 찾기
+ * @header Authorization: login token
+ * @returns {object} title, content, id, createAt, updateAt
+ */
+
+const getTodoById = () => {};
